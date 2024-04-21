331e937920cd9d122f4043abd6c40fd8c514a646
tsukki <lelouch@qieqie.me>
2024-04-21 21:41:38 +0800

    添加导表工具

---
+++ Assets/Scripts/Configs/ItemConfig.cs
@@ -0,0 +1,10 @@
+public class ItemConfig : BaseConfig
+{
+    public string name;
+    public string icon;
+    public int stack;
+    public int effectId;
+    public float price;
+    public int[] test1;
+    public string[] test2;
+}
